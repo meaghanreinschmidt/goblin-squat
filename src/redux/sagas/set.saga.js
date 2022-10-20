3b4319ba8a61ce88330cbed5f2bf47c238169777
Meaghan Reinschmidt <mreinschmidt@hotmail.com>
2022-10-19 20:37:08 -0500

    Add set saga

---
+++ src/redux/sagas/set.saga.js
@@ -0,0 +1,20 @@
+import axios from 'axios';
+import { put, takeLatest } from 'redux-saga/effects';
+
+// READ 
+function* fetchSet() {
+    // get sets from the DB 
+    try {
+        const sets = yield axios.get('/api/set');
+        console.log('get sets', sets.data)
+        yield put ({ type: 'SET_SET', payload: sets.data });
+    } catch (error) {
+        console.log('get set error', error);
+    }
+}
+
+function* setSaga() {
+    yield takeLatest('FETCH_SETS', fetchSet);
+}
+
+export default setSaga;
