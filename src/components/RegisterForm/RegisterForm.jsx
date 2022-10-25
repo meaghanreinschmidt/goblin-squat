2675a64bf9e4aae24314b7c9d70059f4673bbe88
Meaghan Reinschmidt <mreinschmidt@hotmail.com>
2022-10-25 13:17:19 -0500

    after user registers, redirected into app

--- src/components/RegisterForm/RegisterForm.jsx
+++ src/components/RegisterForm/RegisterForm.jsx
@@ -60,7 +60,7 @@ function RegisterForm() {
           type="button"
           className="btn btn_asLink"
           onClick={() => {
-            history.push('/login');
+            history.push('/user');
           }}
         >
           Cancel
