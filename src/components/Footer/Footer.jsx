100f72b008a44d450c82e66815814e66b57bcec9
Meaghan Reinschmidt <mreinschmidt@hotmail.com>
2022-10-24 22:42:19 -0500

    Implement BottomNavigation mui

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 import './Footer.css';
 import { useSelector } from 'react-redux';
+import BottomNavigation from '@mui/material/BottomNavigation';
+import BottomNavigationAction from '@mui/material/BottomNavigationAction';
 import HomeIcon from '@mui/icons-material/Home';
 import GradingIcon from '@mui/icons-material/Grading';
 import LeaderboardIcon from '@mui/icons-material/Leaderboard';
@@ -9,39 +11,39 @@ import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 
 
 function Footer() {
-  const user = useSelector((store) => store.user);
-  return <footer className="App-footer">
-    {/* <Link to="/home">
-        <h2 className="nav-title">Prime Solo Project</h2>
-      </Link> */}
-      <div>
-        {/* If no user is logged in, show these links */}
-        {/* {!user.id && (
-          // If there's no user, show login/registration links
-          <Link className="navLink" to="/login">
-            Login / Register
-          </Link>
-        )} */}
 
+  const user = useSelector((store) => store.user);
+  const history = useHistory();
+
+  const [value, setValue] = React.useState('recents');
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+  console.log('navigation icon clicked:', value);
+
+  return <footer className="App-footer">
+      <div>
         {/* If a user is logged in, show these links */}
         {user.id && (
-          <>
-            <Link className="navLink" to="/user">
-            <HomeIcon className="home-button"></HomeIcon>
-            </Link>
+          <BottomNavigation
+            sx={{ width: 350 }}
+            value={value}
+            onChange={(event, setValue) => handleChange(event, setValue)}  
+          >
+            <BottomNavigationAction label="Home" icon={<HomeIcon />}
+              onClick={() => {history.push('/user')}} />
 
-            <Link className="navLink" to="/workout/log">
-              <GradingIcon className="completed-button"></GradingIcon>
-            </Link>
+            <BottomNavigationAction label="Log" icon={<GradingIcon />}
+              onClick={() => {history.push('/workout/log')}} />
 
-            <Link className="navLink" to="/progress/chart">
-              <LeaderboardIcon className="progress-icon"></LeaderboardIcon>
-            </Link>
+            <BottomNavigationAction label="Progress" icon={<LeaderboardIcon />}
+              onClick={() => {history.push('/progress/chart')}} />
 
-            <Link className="navLink" to="/user/profile">
-              <AccountCircleIcon className="user-icon"></AccountCircleIcon>
-            </Link>
-          </>
+            <BottomNavigationAction label="Profile" icon={<AccountCircleIcon />}
+              onClick={() => {history.push('/user/profile')}} />
+
+          </BottomNavigation>
         )}
 
         {/* <Link className="navLink" to="/about">
