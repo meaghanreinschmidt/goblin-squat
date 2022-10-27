15d5af2cd883235dc2d7661a418c613ba9cda384
Meaghan Reinschmidt <mreinschmidt@hotmail.com>
2022-10-27 13:18:20 -0500

    Dialog box on delete in home page

--- src/components/ActiveWorkoutItem/ActiveWorkoutItem.jsx
+++ src/components/ActiveWorkoutItem/ActiveWorkoutItem.jsx
@@ -1,8 +1,13 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useHistory, useParams } from 'react-router-dom';
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
 import Grid from "@mui/material/Grid";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
@@ -16,6 +21,15 @@ function ActiveWorkoutItem({workout}) {
     const dispatch = useDispatch();
     const history = useHistory();
     const { id } = useParams();
+    const [open, setOpen] = useState(false);
+
+    const handleClickOpen = () => {
+        setOpen(true);
+    }
+
+    const handleClose = () => {
+        setOpen(false);
+    }
 
     const handleDetails = () => {
         console.log('clicked on single workout');
@@ -26,6 +40,7 @@ function ActiveWorkoutItem({workout}) {
     const handleDelete = (id) => {
         console.log('handling delete', id);
         dispatch({ type: 'DELETE_WORKOUT', payload: id});
+        setOpen(false);
     }
 
     const handleComplete = (id) => {
@@ -43,10 +58,32 @@ function ActiveWorkoutItem({workout}) {
                     </Button>
                     <Button>
                         <DeleteIcon
-                            onClick={() => handleDelete(workout.id)}
+                            onClick={handleClickOpen}
+                            // onClick={() => handleDelete(workout.id)}
                             className="delete-icon"
                         ></DeleteIcon>
                     </Button>
+                    <Dialog 
+                        open={open}
+                        onClose={handleClose}
+                        aria-labelledby="alert-dialog-title"
+                        aria-describedby="alert-dialog-description"
+                    >
+                        <DialogTitle id="alert-dialog-title">
+                            {"Delete?"}
+                        </DialogTitle>
+                        <DialogContent>
+                            <DialogContentText id="alert-dialog-description">
+                                Are you sure you want to delete this item?
+                            </DialogContentText>
+                        </DialogContent>
+                        <DialogActions>
+                            <Button onClick={handleClose}>Cancel</Button>
+                            <Button onClick={() => handleDelete(workout.id)} autoFocus>
+                                Delete
+                            </Button>
+                        </DialogActions>
+                    </Dialog>
                     <Button>
                     <CheckCircleOutlineIcon
                         onClick={() => handleComplete(workout.id)}
