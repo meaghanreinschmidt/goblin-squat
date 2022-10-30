57737f0c3f2147e5a395619b31db0f420f081bbc
Meaghan Reinschmidt <mreinschmidt@hotmail.com>
2022-10-30 14:11:30 -0500

    Styled table

--- src/components/ViewExercise/ViewExercise.jsx
+++ src/components/ViewExercise/ViewExercise.jsx
@@ -3,6 +3,19 @@ import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useHistory, useParams } from 'react-router-dom';
 import './ViewExercise.css';
+import Box from '@mui/material/Box';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+const theme = createTheme({
+    palette: {
+      primary: {
+        main: '#2d2d2d'
+      },
+      secondary: {
+        main: '#FA6318'
+      }
+    }
+  })
 
 function ViewExercise() {
     const dispatch = useDispatch();
@@ -21,36 +34,36 @@ function ViewExercise() {
     }, [id]);
 
     return (
-        <>
+        <Box className="container">
+            <ThemeProvider theme={theme}>
             <center>
-                <br />
-                <br />
-                <h4>{exercises.name}</h4>
-                <h5>Sets: </h5>
+                <div className="view-container">
+                <h3 className="App-header">{exercises.name}</h3>
                 <table>
                     <tr>
-                        <th>Set #</th>
-                        <th>Reps</th>
-                        <th>Weight</th>
+                        <th className="table-header">Set #</th>
+                        <th className="table-header">Reps</th>
+                        <th className="table-header">Weight</th>
                     </tr>
                 {sets.map(set => {
                     return (
                         <tr>
-                            <td>{set.set_number}</td>
-                            <td>{set.reps}</td>
-                            <td>{set.weight}</td>
+                            <td className="table-item">{set.set_number}</td>
+                            <td className="table-item">{set.reps}</td>
+                            <td className="table-item">{set.weight}</td>
                         </tr>
                         
                     )
                 })}
                 </table>
                 <br />
-                <h5>Notes: </h5>
                 <p>{exercises.notes}</p>
                 <br />
-                <Button onClick={() => history.goBack()}>Back</Button>
+                <Button variant="contained" onClick={() => history.goBack()}>Back</Button>
+                </div>
             </center>
-        </>
+            </ThemeProvider>
+        </Box>
     )
 }
 
