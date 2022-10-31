fc34d401a7d42b138e375ed0f9893cd711fcfa0f
Meaghan Reinschmidt <mreinschmidt@hotmail.com>
2022-10-30 19:48:40 -0500

    Add header logo

--- src/components/ProgressChart/ProgressChart.jsx
+++ src/components/ProgressChart/ProgressChart.jsx
@@ -9,16 +9,6 @@ function ProgressChart() {
         <div className="chart-container">
         <h2 className="App-header">Progress Chart</h2>
         <p>Coming soon...</p>
-        <br />
-        <h4>Technologies Used</h4>
-        <div>
-          <p>HTML, CSS, JavaScript, Node.js, Express.js, React, Redux, Redux Sagas, Material UI, PostgreSQL</p>
-        </div>
-        <br />
-        <h4>Acknowledgements</h4>
-          <p>My partner Maddie, friends, and family</p>
-          <p>Prime faculty and staff</p>
-          <p>Members of the Phrygian cohort</p>
           </div>
       </center>
     </Box>
